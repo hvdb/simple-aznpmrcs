1774c857534608cbe5daf3d5e1aee78773334b19
Henk van den Brink <henkvandenbrink@gmail.com>
2023-06-30 15:25:32 +0200

    chore: some work

--- src/index.ts
+++ src/index.ts
@@ -20,13 +20,7 @@ const NPMRC       = process.env.NPMRC || join(process.env.HOME || process.env.US
 //     description: "Year number",
 //     demand: true
 // }).argv;
-let userId = '';
 
-function setup() {
-   // We need to determine who is executing this.
-   const output = execSync('whoami');
-   userId = output.toString().replace(/(\r\n|\n|\r)/gm, '');
-}
 
 function createDefault() {
     const defaultNpmrcTemplate = fs.readFileSync(join(__dirname, '../assets/npmrc-default'));
@@ -34,45 +28,36 @@ function createDefault() {
     if (!fs.existsSync(`${NPMRC_STORE}/default`)) {
         // Execute npmrc to create default file.
         execSync('node_modules/.bin/npmrc -c default', { stdio: 'inherit' });
-
+        // Write the template to the default npmrc
         fs.writeFileSync(`${NPMRC_STORE}/default`, defaultNpmrcTemplate);
-        const defaultNpmrOnDisk = fs.readFileSync(`${NPMRC_STORE}/default`);
-        console.log('dd', defaultNpmrOnDisk.toString());
     }
 }
 
-// function createNpmrcs() {
-//     // create default.
-//     createDefault();
-
-//     // load template
-//     const template = fs.readFileSync(join(__dirname, '../assets/npmrc-template')).toString();
-//     projects.forEach((prj) => {
-//         if (!existsSync(`/Users/${userId}/.npmrcs/${prj.project}`)) {
-//             // Create an npmrc with the name of the project
-//             execSync(`npx npmrc -c ${prj.project}`, { stdio: 'inherit' });
-//             // Now we update the template with the feed.
-//             const newNpmrc = template.replaceAll('$NPM_FEED', prj.feed);
-//             // Save the npmrc so that it can be used.
-//             writeFileSync(`/Users/${userId}/.npmrcs/${prj.project}`, newNpmrc);
-//         }
-//     })
-// }
+function createNpmrcs() {
+    
+    // load template for azure
+    const template = fs.readFileSync(join(__dirname, '../assets/npmrc-template')).toString();
+    projects.forEach((prj) => {
+        if (!existsSync(`/Users/${userId}/.npmrcs/${prj.project}`)) {
+            // Create an npmrc with the name of the project
+            execSync(`npx npmrc -c ${prj.project}`, { stdio: 'inherit' });
+            // Now we update the template with the feed.
+            const newNpmrc = template.replaceAll('$NPM_FEED', prj.feed);
+            // Save the npmrc so that it can be used.
+            writeFileSync(`/Users/${userId}/.npmrcs/${prj.project}`, newNpmrc);
+        }
+    })
+}
 
 
 function start() {
-    setup();
+    // create default.
     createDefault();
+    // Create different npmrcs
+    createNpmrcs();
 
 
     console.log('NPMRC', NPMRC);
 }
-
-
-function createNpmrcs() {
-
-}
-
-
 
 start();
