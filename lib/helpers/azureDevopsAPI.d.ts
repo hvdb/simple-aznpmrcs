11bc991df17a8ee0e417c15fe8be0dd72b8d5911
Henk van den Brink <henkvandenbrink@gmail.com>
2023-07-04 22:18:45 +0200

    chore: remove generaed

--- lib/helpers/azureDevopsAPI.d.ts
+++
@@ -1,3 +0,0 @@
-declare function createPat(azOrganization: string, displayName: string): any;
-declare function revokePat(azOrganization: string, authId: string): void;
-export { createPat, revokePat, };
--- lib/helpers/npmrc.d.ts
+++
@@ -1,4 +0,0 @@
-declare function useNpmrc(name: string): void;
-declare function createNpmrcs(feed: string, azProject?: string, azOrg?: string, name?: string): void;
-declare function updateNpmrcWithNewPat(npmrcName?: string): void;
-export { updateNpmrcWithNewPat, createNpmrcs, useNpmrc, };
